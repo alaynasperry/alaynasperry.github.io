6d62e18254b14434cf63e0610ab2a6662923fdbe
alaynasperry <alayna.sperry@gmail.com>
2024-11-25 10:38:15 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-..
+function showit(var1) {
+  console.log(var1)
+  //hide the one div that is shown
+  document.getElementById("home").style.display = "none"
+  document.getElementById("about").style.display = "none"
+  document.getElementById("gallery").style.display = "none"
+  document.getElementById("invest").style.display = "none"
+  document.getElementById("testimonials").style.display = "none"
+  document.getElementById("contact").style.display = "none"
+  
+  document.getElementById(var1).style.display = 'block';
+}
