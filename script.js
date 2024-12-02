8ea552d50588ce7cbdc49358489034ae40367d01
alaynasperry <alayna.sperry@gmail.com>
2024-12-02 15:18:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function showit(page) {
   console.log(page)
   document.getElementById("home").style.display = "none"
@@ -8,6 +9,23 @@ function showit(page) {
   document.getElementById("contact").style.display = "none"
 
   document.getElementById(page).style.display = "block"
+}
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    
+    this.classList.toggle("active");
+
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
 }
 
 var acc = document.getElementsByClassName("accordion");
