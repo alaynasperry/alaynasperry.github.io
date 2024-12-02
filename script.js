8596093d2a82e22f305fbfbfe765ac433fc41e09
alaynasperry <alayna.sperry@gmail.com>
2024-12-02 14:25:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 function showit(page) {
   console.log(page)
-  //hide the one div that is shown
   document.getElementById("home").style.display = "none"
   document.getElementById("about").style.display = "none"
   document.getElementById("gallery").style.display = "none"
   document.getElementById("invest").style.display = "none"
   document.getElementById("testimonials").style.display = "none"
   document.getElementById("contact").style.display = "none"
-  
-  document.getElementById(page).style.display = 'block';
+
+  document.getElementById(page).style.display = "block"
+}
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
 }
