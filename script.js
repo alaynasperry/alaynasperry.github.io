6edf9ae54652de5a09c9b3c262785e1f679aafc6
alaynasperry <alayna.sperry@gmail.com>
2024-12-06 13:39:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -10,34 +10,35 @@ function showit(page) {
   document.getElementById(page).style.display = "block"
 }
 
-var acc = document.getElementsByClassName("accordion");
-var i;
+
+
+var acc = document.getElementsByClassName("accordion")
+var i
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    
-    this.classList.toggle("active");
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active")
 
-    var panel = this.nextElementSibling;
+    var panel = this.nextElementSibling
     if (panel.style.display === "block") {
-      panel.style.display = "none";
+      panel.style.display = "none"
     } else {
-      panel.style.display = "block";
+      panel.style.display = "block"
     }
-  });
+  })
 }
 
-var acc = document.getElementsByClassName("accordion");
-var i;
+var acc = document.getElementsByClassName("accordion")
+var i
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active")
+    var panel = this.nextElementSibling
     if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
+      panel.style.maxHeight = null
     } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
+      panel.style.maxHeight = panel.scrollHeight + "px"
     }
-  });
+  })
 }
